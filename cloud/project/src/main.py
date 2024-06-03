e724044c0104ee50df898c13ea599f0dfdc23591
Shinji13 <keskasaymen8@gmail.com>
2024-06-03 17:25:33 +0100

    cloud project finished

--- cloud/project/src/main.py
+++ cloud/project/src/main.py
@@ -2,8 +2,7 @@ import pymupdf
 import zipfile
 from io import BytesIO
 from typing import List
-from fastapi import FastAPI, UploadFile, Response
-
+from fastapi import FastAPI, UploadFile, Response, File
 
 app = FastAPI()
 
@@ -21,26 +20,29 @@ def main():
 
 
 @app.post("/create_embedded_pdf")
-async def create_embedded_pdf(files: List[UploadFile]):
-    if fileCheck(files):
+async def create_embedded_pdf(files: List[UploadFile], master: UploadFile):
+    if fileCheck(files + [master]):
         return Response("All files must be in pdf format", 403)
-    dummyPdf = pymupdf.open()
-    dummyPdf._newPage(pno=-1, width=595, height=842)
+
+    master_data = await master.read()
+    master_buffer = BytesIO(master_data)
+    dummyPdf = pymupdf.open(stream=master_buffer, filetype="pdf")
+
     for file in files:
         file_data = await file.read()
         file_buffer = BytesIO(file_data)
         dummyPdf.embfile_add(
             buffer_=file_buffer, filename=file.filename, name=file.filename
         )
+
     output = BytesIO()
     dummyPdf.save(output)
     output.seek(0)
-
     return Response(
         content=output.read(),
         status_code=200,
         media_type="application/pdf",
-        headers={"Content-Disposition": "attachment; filename='output.pdf'"},
+        headers={"Content-Disposition": "attachment; filename=output.pdf"},
     )
 
 
@@ -61,5 +63,5 @@ async def extract_embedded_pdf(file: UploadFile):
         content=zip_buffer.read(),
         status_code=200,
         media_type="application/zip",
-        headers={"Content-Disposition": "attachment; filename='pdf.zip'"},
+        headers={"Content-Disposition": "attachment; filename=pdf.zip"},
     )
